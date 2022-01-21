bdad55e9a0e5fce106b51090afdcd59c592ac17c
dris-e <driswas.business@gmail.com>
2022-01-21 09:36:46 -0600

    dhsajhdjkashk touch suptort !!!

--- portfolio/web6.js
+++ portfolio/web6.js
@@ -19,6 +19,24 @@ window.onkeydown = function(e) {
     } 
 };
 
+function touched(arg) {
+    var player = document.getElementById("player");
+    pressed[arg];
+    pressed[arg] = true;
+
+    console.log(pressed);
+    if (pressed["top"] == true && pressed["bottom"] == true) {
+        player.style.marginTop = "44vh";
+        pressed = {}; 
+    } else if (pressed["top"] == true) {
+        player.style.marginTop = "0vh";
+        switchGravity(player, "light");
+    } else if (pressed["bottom"] == true) {
+        player.style.marginTop = "88vh";
+        switchGravity(player, "dark");
+    }
+}
+
 
 function keepDark() {
     var j = 0;
@@ -29,7 +47,6 @@ function keepDark() {
                 color[i].style.backgroundColor = "#1F1F1F";
                 color[i].style.boxShadow = "0px 0px 20px rgba(209, 209, 209, 0.3)";
             }
-            console.log("sus");
             keepDark();     
             j++;
         }
@@ -200,24 +217,6 @@ function newElem() {
     } 
 
     document.getElementById("color" + divNum).style.width = (ranNum / 2) + 10 + "vw";
-    /*
-    else if (ranNum == 20) {
-        addElem(container, "rare", divNum);
-        document.getElementById("color" + divNum).style.marginTop = "54vh";
-        document.getElementById("color" + divNum).innerHTML = "x2"
-    } else if (ranNum == 21) {
-        addElem(container, "rare", divNum);
-        document.getElementById("color" + divNum).style.marginTop = "-2vh";
-        document.getElementById("color" + divNum).innerHTML = "x2"
-    } else if (ranNum == 19) {
-        addElem(container, "bad", divNum);
-        document.getElementById("color" + divNum).style.marginTop = "-2vh";
-        document.getElementById("color" + divNum).innerHTML = "/2"
-    } else if (ranNum == 22) {
-        addElem(container, "bad", divNum);
-        document.getElementById("color" + divNum).style.marginTop = "-2vh";
-        document.getElementById("color" + divNum).innerHTML = "/2"
-    } */
 
     return arr;
 }
@@ -229,201 +228,3 @@ function addElem(cont, name, num) {
     cont.appendChild(newElem);
     arr.push("color" + num);
 }
-
-/* function getNum() {
-    ranNum = (Math.floor(Math.random() * 10) + 1),
-    arr = newElem();
-
-    for (let i = 0; i < arr.length; i++) {
-        var ranDec = Math.floor(Math.random() * 4),
-        ranVar = Math.floor((Math.random() * 10) + 1),
-        player = document.getElementById("player"),
-        front;
-
-        if (ranDec === 0) {
-            front = "x";
-        } else if (ranDec === 1) {
-            front = "+";
-        } else if (ranDec === 2) {
-            front = "/";
-        } else {
-            front = "-";
-        }
-
-        var elem = document.getElementById(arr[i]);
-        elem.innerHTML = front + ranVar;
-
-        returnVar(front, ranVar);
-    }
-
-
-} */
-
-/* function isTouching() {
-    var player = document.getElementById("player"),
-    pos,
-    points = 0,
-    total = 3;
-
-    /* switch () {
-        case 0:
-            pos = 15;
-            break;
-        case 1:
-            pos = 75;
-            break;
-        case 2:
-            pos =  45;
-            break;
-        case 3:
-            pos = 65;
-            break;
-    }
-
-    var currentPos = Math.round(parseInt(getComputedStyle(player).marginTop) * (100 / document.documentElement.clientHeight));
-
-    if (currentPos === pos) {
-        points++;
-    } else {
-        total--;
-    }
-    console.log(currentPos);
-    console.log(points);
-    return points;
-} */
-
-/*
-            if (ranVar / 10 > parseInt(getComputedStyle(player).scale)) {
-                player.style.scale = parseInt(getComputedStyle(player).scale) * ranVar;
-            } else {
-                player.style.scale = parseInt(getComputedStyle(player).scale) * ranVar / 100;
-            }
-*/
-
-
-
-
-
-
-
-
-/*
-var pressed = {};
-var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "grey"];
-
-window.onkeydown = function(e) {
-    var player = document.getElementById("player");
-    switch (e.keyCode) {
-        case 68:
-            player.style.marginTop = "0vh";
-            checkPoints();
-            changeColor(newRanVar);
-            break;
-        case 70:
-            player.style.marginTop = "20vh";
-            checkPoints();
-            changeColor(newRanVar);
-            break;           
-        case 74:
-            player.style.marginTop = "45vh";
-            checkPoints();
-            changeColor(newRanVar);
-            break;
-        case 75:
-            player.style.marginTop = "65vh";
-            checkPoints();
-            changeColor(newRanVar);
-            break;
-    }
-};
-
-function randomColor() {
-    var r = Math.floor(Math.random() * 9);
-    var ranCol = colors[r];
-    return ranCol;
-}
-
-function ranVar() {
-    var ranVar = Math.floor(Math.random() * 4);
-    return ranVar;
-}
-
-function newVar() {
-    newRanVar = ranVar();
-}
-
-var newRanVar = ranVar();
-console.log(newRanVar);
-
-var i = 5000;
-function changeColor(arg) {
-    i -= 100;
-    var arr = [];
-    var ranVar = arg;
-    for (let i = 0; i < 4; i++) {
-        var elem = document.getElementsByClassName("color");
-        var player = document.getElementById("player");
-        var playerText = document.getElementById("player-text");
-
-        var ranNum = randomColor(); 
-        elem[i].style.backgroundColor = ranNum;
-
-        player.style.backgroundColor = randomColor();
-        playerText.innerHTML = randomColor();
-
-        arr.push(ranNum);
-
-        if (i === ranVar) {
-            playerText.style.color = arr[ranVar];
-        } 
-    }
-    var container = document.getElementById("container-1");
-    container.style.marginLeft = "-10vw";
-    container.style.transition = "margin-left, " + i; 
-    setTimeout(function(){container.style.marginLeft = "90vw";}, i);
-
-} 
-
-var passed = 0;
-var lives = 3;
-
-function checkPoints() {
-    var player = document.getElementById("player");
-    var pos;
-
-    switch (newRanVar) {
-        case 0:
-            pos = 0;
-            break;
-        case 1:
-            pos = 20;
-            break;
-        case 2:
-            pos =  45;
-            break;
-        case 3:
-            pos = 65;
-            break;
-    }
-
-    var currentPos = parseInt(getComputedStyle(player).marginTop) * (100 / document.documentElement.clientHeight);;
-
-    console.log(currentPos);
-    console.log(pos);
-
-    if (currentPos === pos) {
-        passed += 2;
-        lives++;
-        newVar();
-    } else {
-        lives--;
-    }
-    
-    document.getElementById("game-score").innerHTML = "score: " + passed;
-    document.getElementById("game-lives").innerHTML = "lives: " + lives;
-}
-*/
-
-
-
-
